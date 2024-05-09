047e7ffd70fe7487324daa63a79ceea8cb0ff4f3
calebetadeumatosdasilva <calebetadeu@gmail.com>
2024-05-09 15:30:45 -0300

    Configufando o firebase

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:to_do_list_provider/app/core/ui/todo_list_ui_config.dart';
 import 'package:to_do_list_provider/app/modules/auth/auth_module.dart';
 import 'package:to_do_list_provider/app/modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+  @override
+  void initState() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    WidgetsBinding.instance?.addObserver(sqliteAdmConnection);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
